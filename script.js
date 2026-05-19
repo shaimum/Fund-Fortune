e7c513751c93cfa4bd73a9e5ffcaa7d82ab752cd
Sagor <67408763+shaimum@users.noreply.github.com>
2026-05-19 16:36:18 +0600

    Set SCRIPT_URL to deployed GAS Web App
    Updated SCRIPT_URL to the deployed GAS Web App URL.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = ''; // Provide GAS Web App URL here after deployment
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby_lhXpD2xr6Iz2N361WCxIi3LKriDkGQZbTZMYOs5Ctfjkw7yAaJEE7_V-GSq7RoT63g/exec'; // Provide GAS Web App URL here after deployment
 
 // Define authorized emails here (Optional security layer)
 const AUTHORIZED_EMAILS = []; // Leave empty to allow any Google account, or add ['user1@gmail.com', 'user2@gmail.com']
